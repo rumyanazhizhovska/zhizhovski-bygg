e9337e685309c479fb59c7f417eb60d03ea96591
Rumyana <rzhizhovska@gmail.com>
2026-06-30 01:10:31 +0200

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,22 @@
+import styles from "./page.module.css";
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <div className={styles.container}>
+        <h1>Velkommen! 👋</h1>
+        <p>
+          Du kan se koden for dette prosjektet{" "}
+          <a
+            className={styles.link}
+            href="https://github.com/bekk/prosjektutgangspunkt"
+          >
+            i repoet på GitHub
+          </a>
+        </p>
+        <p>
+          Du kan endre koden i <code className={styles.code}>src/App.jsx</code>
+        </p>
+      </div>
+    </main>
+  );
+}
