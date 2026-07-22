778b68c69e989263ed923800c61844c577d5104e
Rumyana <rzhizhovska@gmail.com>
2026-07-22 21:01:41 +0300

    Add google search console verification

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,9 @@ import Footer from "../components/Footer/Footer";
 export const metadata: Metadata = {
   title: "Zhizhovski Bygg",
   description: "Velkommen til Zhizhovski Bygg sitt nettsted. Vi tilbyr tjenester innen bygg og anlegg, og vi er dedikert til å levere kvalitetsarbeid til våre kunder.",
+
+  verification: {
+    google: "1ZNr5tlIQ9sLHmUvQGLKoQR7ke7AsxjdtjS7mtSTook"
 };
 
 export default function RootLayout({
