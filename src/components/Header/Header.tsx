089e5032349b82df69bb9d3b08149316b142f496
Rumyana <rzhizhovska@gmail.com>
2026-07-31 01:05:06 +0300

    feat: move to home when clicked on logo

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,19 +1,26 @@
+import Link from "next/link";
 import styles from "./Header.module.css";
 import Menu from "../Menu/Menu";
 
 function Header() {
-    return (
-        <header className={styles.header}>
-            <h1 className={styles.logo}>
-                <img
-                    className={styles.img}
-                    src="/logo-bc-transparent.svg"
-                    alt="Zhizhovski Bygg"
-                />
-            </h1>
-            <Menu/>
-        </header>
-    );
+  return (
+    <header className={styles.header}>
+      <Link
+        href="/"
+        className={styles.logo}
+        aria-label="Zhizhovski Bygg - hjem"
+      >
+        <img
+          className={styles.img}
+          src="/logo-bc-transparent.svg"
+          alt="Zhizhovski Bygg"
+          width="400"
+          height="225"
+        />
+      </Link>
+      <Menu />
+    </header>
+  );
 }
 
 export default Header;
