1c164b6ba6a8dd10388847d974f6e77394d586ca
Rumyana <rzhizhovska@gmail.com>
2026-08-01 15:42:04 +0200

    add a css file for this page and make some changes

--- src/app/(pages)/contact/page.tsx
+++ src/app/(pages)/contact/page.tsx
@@ -2,15 +2,18 @@ import ContactForm from "@/components/ContactForm/ContactForm";
 import styles from "../../page.module.css";
 
 export default function Contact() {
-    return (
-        <main className={styles.main}>
-            <div className={styles.container}>
-                <div className={styles.header}>
-                    <h2>Kontaktskjema</h2>
-                    <p>Send inn oppdraget ditt, så tar vi en prat om omfang, tid og løsning.</p>
-                </div>
-                <ContactForm />
-            </div>
-        </main>
-    );
+  return (
+    <main className={styles.main}>
+      <div className={styles.container}>
+        <div className={styles.header}>
+          <h1>Kontaktskjema</h1>
+          <p>
+            Send inn oppdraget ditt, så tar vi en prat om omfang, tid og
+            løsning.
+          </p>
+        </div>
+        <ContactForm />
+      </div>
+    </main>
+  );
 }
